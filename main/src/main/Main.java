f62c78c082b2bcaa5e6eec464856271497dbd3e3
sdac-9 <sdac-9@sdac-9-PC>
2024-04-27 09:15:23 +0530

    commit 3rd

--- main/src/main/Main.java
+++ main/src/main/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[]args){
 		System.out.println("Hello Worid");
 		System.out.println("Hello github");
+		System.out.println("this is first practical in lab");
+		
 	}
 }
